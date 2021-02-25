115e0ae8d186fd787d93f2ed905346b4966b0bbf
DomatesGames <74550237+DomatesGames@users.noreply.github.com>
2021-02-25 18:16:45 +0200

    Add files via upload

---
+++ source-code/botinvitationmaker-en.py
@@ -0,0 +1,6 @@
+print("Welcome to Bot Invitation Maker!")
+BOT_ID = input("Enter your bot's Client ID (CTRL + V to paste.): ")
+print("Preparing link...")
+print("https://discord.com/oauth2/authorize?client_id=" + BOT_ID + "&scope=bot&permissions=8" + " (CTRL + C to copy.)")
+print("Thanks for using Bot Invitation Maker.")
+input()
---
+++ source-code/botinvitationmaker-tr.py
@@ -0,0 +1,6 @@
+print("Bot Invitation Maker'a hoşgeldiniz!")
+BOT_ID = input("Botunuzun Client ID'sini giriniz (Yapıştırmak için CTRL + V): ")
+print("Link hazırlanıyor...")
+print("https://discord.com/oauth2/authorize?client_id=" + BOT_ID + "&scope=bot&permissions=8" + " (Kopyalamak için CTRL + C)")
+print("Bot Invitation Maker'ı kullandığınız için teşekkür ederiz.")
+input()
